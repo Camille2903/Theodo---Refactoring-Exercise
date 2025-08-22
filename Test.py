1a3c7246a6443c6bdeedd9deb29a2055ca26f6c2
Camille_FRUCTUOSO <camille.fructuoso291@gmail.com>
2025-08-22 15:28:19 +0200

    2

--- Test.py
+++ Test.py
@@ -2,3 +2,16 @@ def more():
     return "more"
 
 more()
+
+
+
+def test_abs_for_a_negative_number():
+ 
+  # Arrange
+  negative = -2
+   
+  # Act
+  answer = abs(negative)
+   
+  # Assert
+  assert answer == 2
